8406adc1b218e2750a35158393a3325bf3fbb31d
SUYASH NARESH MADHAVI <64771490+SUYASH-a17@users.noreply.github.com>
2025-01-29 00:13:39 -0500

    Add files via upload

---
+++ Linked List/LL_kth_node_from_end.py
@@ -0,0 +1,59 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+        
+def find_kth_from_end(self, k):
+    temp = pre = self.head
+    for _ in range(k):
+        if temp is None:
+            return None
+        temp = temp.next
+    while temp:
+        pre = pre.next
+        temp = temp.next
+    return pre
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+my_linked_list.append(6)
+my_linked_list.append(7)
+my_linked_list.append(8)
+
+
+k = 3
+result = find_kth_from_end(my_linked_list, k)
+
+print(result.value)  # Output: 4
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    4
+    
+"""
+
